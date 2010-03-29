ff8a801a7a9996775d02fc863e75da9a39b6afc8
Marco Maggi <marco.maggi-ipsu@poste.it>
2010-03-29 17:37:53 +0200

    changed the rebuild-and-test script

--- rebuild-and-test-project.sh
+++ rebuild-and-test-project.sh
@@ -2,6 +2,8 @@
 ##
 ##
 
+set -e
+
 PROJECT=${1:?missing project pathname}
 
 if test -d "$PROJECT"
@@ -11,11 +13,34 @@ then
     if test -f prepare.sh
     then
         echo "--- found prepare.sh"
+        if test -f ./make-inspector.sps
+        then
+            echo "--- found make-inspector.sps"
+            ./make-inspector.sps
+        fi
+
         test -d "=build" || mkdir "=build"
         cd "=build"
-        test -f Makefile && yes yes | make clean-builddir
+        if test -f Makefile
+        then yes yes | make clean-builddir
+        fi
         sh ../prepare.sh
-        make -k all test
+        make dist
+        PKG_ID=$(make echo-variable VARIABLE=PKG_ID)
+        DESTDIR=$(make echo-variable VARIABLE=ds_dist_DESTDIR)
+        ARCHIVE=$(make echo-variable VARIABLE=ds_dist_ARCHIVE)
+        test -d ~/var/build/nausicaa || mkdir -p ~/var/build/nausicaa
+        cp "$DESTDIR/$ARCHIVE" ~/var/build/nausicaa
+        (set -e
+            cd ~/var/build/nausicaa
+            tar --extract --bzip2 --file="$ARCHIVE"
+            cd "$PKG_ID"
+            mkdir "=build"
+            cd "=build"
+            sh ../prepare.sh
+            make abu
+            make fasl-installed
+            make test-installed)
     else
         echo "*** prepare.sh not found, aborting"
         exit 2
